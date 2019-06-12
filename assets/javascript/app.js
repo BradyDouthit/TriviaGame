6d53825ba1e652a3a82a819645a4c5067900c249
Brady Douthit <brady.douthit99@gmail.com>
2019-06-12 13:05:22 -0400

    Added a 30 second countdown timer.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,12 @@
-console.log("test")
+var time = 30;
+var seconds = setInterval(myCountDown, 100);
+function myCountDown() {
+    if (time > 0) {
+        time--
+        $("#test").html(time);
+    }
+    else {
+        clearTimeout(seconds);
+        $("#test").html("You took too long!");
+    }
+}
