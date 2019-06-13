4faef22c8f4a10224b53e9cb1d45d1359bee02de
Brady Douthit <brady.douthit99@gmail.com>
2019-06-13 12:32:53 -0400

    Added a message that shows the correct answer.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,10 @@ var time = 0;
 var rightAnswersCount = 0;
 var wrongAnswersCount = 0;
 var count;
-
+var wins = 0;
+var losses = 0;
+var unanswered = 0;
+//this is the timer
 function myCountDown() {
     if (time > 0) {
         time--
@@ -14,6 +17,7 @@ function myCountDown() {
         $(".snoozed").show();
     }
 }
+//resets the timer and shows the respective question when called
 function secondQuestion() {
     time = 30;
     $(".hidden-message-right").hide();
@@ -22,6 +26,7 @@ function secondQuestion() {
     clearTimeout(secondQuestion);
     count = setInterval(myCountDown, 1000);
 }
+//resets the timer and shows the respective question when called
 function thirdQuestion () {
     time = 30;
     $(".hidden-message-right").hide();
@@ -30,10 +35,21 @@ function thirdQuestion () {
     clearTimeout(secondQuestion);
     count = setInterval(myCountDown, 1000);
 }
+//stops the timer
 function stopCount() {
     clearInterval(count)
 }
-
+//adds up wins and losses
+ function winLoss() {
+     if (wins === 5) {
+        $(".wins").html(wins);
+        $(".losses").show();
+     }
+     else {
+         wins++
+     }
+ };
+ //Start button events(shows the first question and starts the timer)
 $("#start-button").on("click", function() {
     count = setInterval(myCountDown, 1000);
     time = 30;    
@@ -43,7 +59,7 @@ $("#start-button").on("click", function() {
 });
 
 
-
+//2nd question right and wrong answer events
 $(".right-answer").on("click", function() {
     $(".QandA").hide();
     $(".hidden-message-right").show();
@@ -52,13 +68,15 @@ $(".right-answer").on("click", function() {
 });
 $(".wrong-answer").on("click", function() {
     $(".QandA").hide();
+    $(".if-wrong").show();
+    $(".if-wrong").html("The correct answer was:" + " D. 16");
     $(".hidden-message-wrong").show();
     setTimeout(secondQuestion, 3000);
     stopCount();
 });
 
 
-
+//3rd question right and wrong answer events
 $(".right-answer2").on("click", function() {
     $(".QandA").hide();
     $(".hidden-message-right").show();
@@ -67,13 +85,14 @@ $(".right-answer2").on("click", function() {
 });
 $(".wrong-answer2").on("click", function() {
     $(".QandA").hide();
+    $(".if-wrong").html("The correct answer was:" + " B. 5000");
     $(".hidden-message-wrong").show();
     setTimeout(thirdQuestion, 3000);
     stopCount();
 });
 
 
-
+//4th question right and wrong answer events will go here
 $(".right-answer3").on("click", function() {
     $(".QandA").hide();
     $(".hidden-message-right").show();
@@ -82,6 +101,7 @@ $(".right-answer3").on("click", function() {
 });
 $(".wrong-answer3").on("click", function() {
     $(".QandA").hide();
+    $(".if-wrong").html("The correct answer was:" + " C. Miraak's Sword");
     $(".hidden-message-wrong").show();
     setTimeout(thirdQuestion, 3000);
     stopCount();
