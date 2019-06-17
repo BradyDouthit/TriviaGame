9ac6a014e928bd7838ccb7c349197c9639ade0a3
Brady Douthit <brady.douthit99@gmail.com>
2019-06-17 17:45:04 -0400

    Questions unanswered will now go to next question.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,64 @@
-var time = 0;
+var time = 30;
 var rightAnswersCount = 0;
 var wrongAnswersCount = 0;
 var count;
 var wins = 0;
 var losses = 0;
 var unanswered = 0;
+var questionsAnswered = 0;
 //this is the timer
 function myCountDown() {
     if (time > 0) {
         time--
         $("#countdown").html("Time left: " + time);
     }
-    else {
+    if (time === 0) {
+        questionsAnswered++
         clearInterval(count)
         $(".QandA").hide();
         $(".snoozed").show();
+        $(".yeet").html(questionsAnswered)
+    }
+    if (questionsAnswered === 1 && time === 0) {
+        clearInterval(count);
+        setTimeout(secondQuestion, 3000)
+        $(".QandA").hide();
+        $(".snoozed").show();
+        unansweredCount();
+    }
+    if (questionsAnswered === 2 && time === 0) {
+        clearInterval(count);
+        setTimeout(thirdQuestion, 3000)
+        $(".QandA").hide();
+        $(".snoozed").show();
+        unansweredCount();
+    }
+    if (questionsAnswered === 3 && time === 0) {
+        clearInterval(count);
+        setTimeout(fourthQuestion, 3000)
+        $(".QandA").hide();
+        $(".snoozed").show();
+        unansweredCount();
+    }
+    if (questionsAnswered === 4 && time === 0) {
+        clearInterval(count);
+        setTimeout(fifthQuestion, 3000)
+        $(".QandA").hide();
+        $(".snoozed").show();
+        unansweredCount();
+    }
+    if (questionsAnswered === 5 && time === 0) {
+        clearInterval(count);
+        setTimeout(lastQuestion, 3000)
+        unansweredCount();
     }
 }
-
-
 //resets the timer and shows the respective question when called
 function secondQuestion() {
     time = 30;
     $(".hidden-message-right").hide();
     $(".hidden-message-wrong").hide();
+    $(".snoozed").hide();
     $(".hidden-question2").show();
     clearTimeout(secondQuestion);
     count = setInterval(myCountDown, 1000);
@@ -33,6 +68,7 @@ function thirdQuestion () {
     time = 30;
     $(".hidden-message-right").hide();
     $(".hidden-message-wrong").hide();
+    $(".snoozed").hide();
     $(".hidden-question3").show();
     clearTimeout(secondQuestion);
     count = setInterval(myCountDown, 1000);
@@ -42,6 +78,7 @@ function fourthQuestion () {
     time = 30;
     $(".hidden-message-right").hide();
     $(".hidden-message-wrong").hide();
+    $(".snoozed").hide();
     $(".hidden-question4").show();
     clearTimeout(thirdQuestion);
     count = setInterval(myCountDown, 1000);
@@ -51,6 +88,7 @@ function fifthQuestion () {
     time = 30;
     $(".hidden-message-right").hide();
     $(".hidden-message-wrong").hide();
+    $(".snoozed").hide();
     $(".hidden-question5").show();
     clearTimeout(fourthQuestion);
     count = setInterval(myCountDown, 1000);
@@ -59,6 +97,7 @@ function fifthQuestion () {
 function lastQuestion () {
     $(".hidden-message-right").hide();
     $(".hidden-message-wrong").hide();
+    $(".snoozed").hide();
     $(".win-loss").show();
     clearTimeout(fifthQuestion);
 }
@@ -100,20 +139,22 @@ function stopCount() {
 
 
 
-
  //Start button events(shows the first question and starts the timer)
 $("#start-button").on("click", function() {
-    count = setInterval(myCountDown, 100);
+    questionsAnswered = 0;
+    count = setInterval(myCountDown, 1000);
     time = 30;    
     $("#start-button").hide();
     $(".hidden").show();
     $(".snoozed").hide();
     $(".win-loss").hide();
+    unansweredCount();
 });
 
 
 //1st question right and wrong answer events
 $(".right-answer").on("click", function() {
+    questionsAnswered++
     $(".QandA").hide();
     $(".hidden-message-right").show();
     setTimeout(secondQuestion, 3000);
@@ -121,6 +162,7 @@ $(".right-answer").on("click", function() {
     winCount();
 });
 $(".wrong-answer").on("click", function() {
+    questionsAnswered++;
     $(".QandA").hide();
     $(".if-wrong").show();
     $(".if-wrong").html("The correct answer was:" + " D. 16");
@@ -133,6 +175,8 @@ $(".wrong-answer").on("click", function() {
 
 //2nd question right and wrong answer events
 $(".right-answer2").on("click", function() {
+    questionsAnswered++;
+    $(".yeet").html(questionsAnswered)
     $(".QandA").hide();
     $(".hidden-message-right").show();
     setTimeout(thirdQuestion, 3000);
@@ -140,6 +184,8 @@ $(".right-answer2").on("click", function() {
     winCount();
 });
 $(".wrong-answer2").on("click", function() {
+    questionsAnswered++;
+
     $(".QandA").hide();
     $(".if-wrong").html("The correct answer was:" + " B. 5000");
     $(".hidden-message-wrong").show();
@@ -151,6 +197,7 @@ $(".wrong-answer2").on("click", function() {
 
 //3rd question right and wrong answer events will go here
 $(".right-answer3").on("click", function() {
+    questionsAnswered++;
     $(".QandA").hide();
     $(".hidden-message-right").show();
     setTimeout(fourthQuestion, 3000);
@@ -158,6 +205,7 @@ $(".right-answer3").on("click", function() {
     winCount();
 });
 $(".wrong-answer3").on("click", function() {
+    questionsAnswered++;
     $(".QandA").hide();
     $(".if-wrong").html("The correct answer was:" + " C. Miraak's Sword");
     $(".hidden-message-wrong").show();
@@ -169,6 +217,7 @@ $(".wrong-answer3").on("click", function() {
 
 //4th question right and wrong answer events will go here
 $(".right-answer4").on("click", function() {
+    questionsAnswered++;
     $(".QandA").hide();
     $(".hidden-message-right").show();
     setTimeout(fifthQuestion, 3000);
@@ -176,6 +225,8 @@ $(".right-answer4").on("click", function() {
     winCount();
 });
 $(".wrong-answer4").on("click", function() {
+    questionsAnswered++;
+
     $(".QandA").hide();
     $(".if-wrong").html("The correct answer was:" + " A. Mehrune's Razor");
     $(".hidden-message-wrong").show();
@@ -188,6 +239,7 @@ $(".wrong-answer4").on("click", function() {
 
 //last question events
 $(".right-answer5").on("click", function() {
+    questionsAnswered++;
     $(".QandA").hide();
     stopCount();
     winCount();
@@ -196,6 +248,7 @@ $(".right-answer5").on("click", function() {
     setTimeout(unansweredCount, 30000);
 });
 $(".wrong-answer5").on("click", function() {
+    questionsAnswered++;
     $(".QandA").hide();
     $(".if-wrong").html("The correct answer was:" + " B. 9");
     stopCount();
@@ -204,7 +257,3 @@ $(".wrong-answer5").on("click", function() {
     setTimeout(lastQuestion, 3000)
     setTimeout(unansweredCount, 30000);
 });
-//REMEMBER TO PUT THESE IN FOR THE LAST QUESTION
-//$(".wins").show();
-//$(".losses").show();
-
